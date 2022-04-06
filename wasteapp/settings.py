3cba06e8bc6787cd75aefd557470e59bb99b51c5
Olumide Oderinde <43780645+o3cloudng@users.noreply.github.com>
2022-04-06 11:44:39 +0100

    Update settings.py

--- wasteapp/settings.py
+++ wasteapp/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'o5qj7a%@yz0@jo6mtqhu^ktzuy-jg*m2^o#7hw7nxtdyv5kw^f'
+SECRET_KEY = 'SECRET'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
